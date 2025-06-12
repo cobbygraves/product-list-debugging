56b6d1644def386dc114b64509102aead8ef8c7e
cobbygraves <rmeocnis@hotmail.com>
2025-06-12 10:44:53 +0000

    add cart items images as part of the modal confirmation

--- src/app/confirm-modal/confirm-modal.component.ts
+++ src/app/confirm-modal/confirm-modal.component.ts
@@ -1,13 +1,32 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { CartItem } from '../models/dessert';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-confirm-modal',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './confirm-modal.component.html',
   styleUrl: './confirm-modal.component.scss',
 })
 export class ConfirmModalComponent {
   startOrder = output();
+  cartService = inject(CartService);
+  cartItems: CartItem[] = [];
+  cartTotal: number = 0;
+
+  constructor() {
+    this.cartService.cartItems$.subscribe({
+      next: (value) => {
+        this.cartItems = value;
+
+        this.cartTotal = value.reduce((previousItem, currentItem) => {
+          return previousItem + currentItem.price * currentItem.qty;
+        }, 0);
+      },
+    });
+  }
+
   handleStartOrder() {
     this.startOrder.emit();
   }
